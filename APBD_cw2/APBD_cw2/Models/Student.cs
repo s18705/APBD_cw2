5d534c284825afd5806fc1b569deff483e1f28be
Karolina <61821714+s18705@users.noreply.github.com>
2020-11-07 16:26:45 +0100

    pierwsze commity

--- APBD_cw2/APBD_cw2/Models/Student.cs
+++ APBD_cw2/APBD_cw2/Models/Student.cs
@@ -7,21 +7,31 @@ namespace APBD_cw2.Models
 {
     public class Student
     {
-        [XmlAttribute(attributeName: "email")]
+        [XmlAttribute(attributeName: "Email")]
         public string email { get; set; }
-        [XmlAttribute(attributeName: "firstname")]
+
+        [XmlAttribute(attributeName: "Imie")]
         public string imie { get; set; }
-        [XmlAttribute(attributeName: "Studies")]
-        public string lastName { get; set; }
-        [XmlAttribute(attributeName: "tryb")]
+
+        [XmlAttribute(attributeName: "Nazwisko")]
+        public string nazwisko { get; set; }
+
+        [XmlAttribute(attributeName: "Tryb")]
         public string tryb { get; set; }
-        [XmlAttribute(attributeName: "id")]
+
+        [XmlAttribute(attributeName: "Id")]
         public string id { get; set; }
-        [XmlAttribute(attributeName: "mothersname")]
-        public string mothersName { get; set; }
-        [XmlAttribute(attributeName: "fathersname")]
-        public string fathersName { get; set; }
-        public string birthDate { get; set; }
+
+        [XmlAttribute(attributeName: "Imie_Matki")]
+        public string imieMatki { get; set; }
+
+        [XmlAttribute(attributeName: "Imie_Ojca")]
+        public string imieOjca { get; set; }
+
+        [XmlAttribute(attributeName: "Data_urodzenia")]
+        public string dataUrodzenia { get; set; }
+
+        [XmlAttribute(attributeName: "Studia")]
         public string studia { get; set; }
     }
 }
--- APBD_cw2/APBD_cw2/Program.cs
+++ APBD_cw2/APBD_cw2/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Xml.Serialization;
 
 namespace APBD_cw2
@@ -11,50 +12,72 @@ namespace APBD_cw2
     {
         static void Main(string[] args)
         {
-            if(args.Length != 3)
+            try
             {
-                Console.WriteLine("Niewystarczajace parametry");
-            }
-
-            var filePath = args[0];
-            var outputPath = args[1];
-
-
-            var students = new HashSet<Student>();
-            foreach (var line in File.ReadAllLines(filePath))
-            {
-                var split = line.Split(",");
-                if (split.Length != 9)
+//
+                if (args.Length != 3)
                 {
+                    Console.WriteLine("Niewystarczajace parametry");
+                }
+
+                var filePath = args[0];
+                var outputPath = args[1];
+                var format = args[2];
+
+                var students = new HashSet<Student>();
+                foreach (var line in File.ReadAllLines(filePath))
+                {
+                    var split = line.Split(",");
+                    if (split.Length != 9)
+                    {
+                        return;
+                    }
+
+                    students.Add(new Student
+                    {
+                        id = $"s{split[4]}",
+                        imie = split[0],
+                        nazwisko = split[1],
+                        tryb = split[2],
+                        studia = split[3],
+                        email = split[6],
+                        imieMatki = split[7],
+                        imieOjca = split[8],
+                        dataUrodzenia = DateTime.Parse(split[5]).ToShortDateString(),
+                    });
+                }
+
+                Console.WriteLine(students.Count);
+                Console.WriteLine(outputPath);
+
+                using (StreamWriter streamWriter = new StreamWriter(outputPath))
+                {
+                    switch (format)
+                    {
+                        case "xml":
+                            Console.WriteLine("halo? ");
+                            XmlSerializer serializer = new XmlSerializer(typeof(HashSet<Student>),
+                                                   new XmlRootAttribute("uczelnia"));
+                            serializer.Serialize(streamWriter, students);
+                            //serializer.Serialize(streamWriter, students);
+
+                            //streamWriter.Write("dupa nie marudz");
+                            // streamWriter.Flush();
+
+                            break;
+
+                        case "json":
+                            var toJson = JsonSerializer.Serialize(students, new JsonSerializerOptions { WriteIndented = true });
+                            streamWriter.Write(toJson);
+                            break;
+
+                    }
 
                 }
-                students.Add(new Student
-                {
-                    id = $"s{split[4]}",
-                    imie = split[0],
-                    lastName = split[1],
-                    tryb = split[2],
-                    studia = split[3],
-                    email = split[6],
-                    mothersName = split[7],
-                    fathersName = split[8],
-                    birthDate = DateTime.Parse(split[5]).ToShortDateString(),
-                });
             }
-
-            Console.WriteLine(students.Count);
-            Console.WriteLine(outputPath);
-
-            using (StreamWriter sw = new StreamWriter(outputPath))
+            catch(Exception e)
             {
-                Console.WriteLine("halo? ");
-                XmlSerializer serializer = new XmlSerializer(typeof(HashSet<Student>),
-                                       new XmlRootAttribute("uczelnia"));
-                serializer.Serialize(sw, students);
-                serializer.Serialize(sw, students);
-
-                sw.Write("dupa nie marudz");
-                sw.Flush();
+                Console.WriteLine(e);
             }
         }
     }
